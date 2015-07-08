197c43b0a8903fdba3cb0848182a8ad79a83248f
Mykola Golub <mgolub@mirantis.com>
2015-07-08 11:32:51 +0300

    Replacing ceph-deploy with stackforge/puppet-ceph: osd classes
    Change-Id: I67fb6b53fac860279b3e3a4b2eeef2f239f36965

--- deployment/puppet/ceph_fuel/manifests/osds.pp
+++ deployment/puppet/ceph_fuel/manifests/osds.pp
@@ -1,6 +1,8 @@
 # prepare and bring online the devices listed in $::ceph_fuel::osd_devices
 class ceph_fuel::osds (
   $devices = $::ceph_fuel::osd_devices,
+  $primary_mon = $::ceph_fuel::primary_mon,
+  $keyring_path = '/var/lib/ceph/bootstrap-osd/ceph.keyring',
 ){
 
   firewall { '011 ceph-osd allow':
@@ -10,6 +12,13 @@ class ceph_fuel::osds (
     action => accept,
   } ->
 
-  ceph_fuel::osds::osd{ $devices: }
+  # XXXMG: not sure this the best way to distribute bootstrap-osd keyring
+  exec { "Populate client.bootstrap-osd keyring":
+    command => "ceph auth get-or-create client.bootstrap-osd > ${keyring_path}",
+    creates => $keyring_path
+  } ->
 
+  file { $keyring_path: mode => '0640', } ->
+
+  ceph_fuel::osds::osd{ $devices: }
 }
--- deployment/puppet/ceph_fuel/manifests/osds/osd.pp
+++ deployment/puppet/ceph_fuel/manifests/osds/osd.pp
@@ -1,30 +1,8 @@
 define ceph_fuel::osds::osd () {
 
-  $deploy_device_name = "${::hostname}:${name}"
+  $device = split($name, ':')
 
-  exec { "ceph-deploy osd prepare ${deploy_device_name}":
-    # ceph-deploy osd prepare is ensuring there is a filesystem on the
-    # disk according to the args passed to ceph.conf (above).
-    #
-    # It has a long timeout because of the format taking forever. A
-    # resonable amount of time would be around 300 times the length of
-    # $osd_nodes. Right now its 0 to prevent puppet from aborting it.
-    command   => "ceph-deploy osd prepare ${deploy_device_name}",
-    returns   => 0,
-    timeout   => 0, # TODO: make this something reasonable
-    tries     => 2, # This is necessary because of race for mon creating keys
-    try_sleep => 1,
-    logoutput => true,
-    unless    => "grep -q ${name} /proc/mounts",
-  } ->
-
-  exec { "ceph-deploy osd activate ${deploy_device_name}":
-    command   => "ceph-deploy osd activate ${deploy_device_name}",
-    try_sleep => 10,
-    tries     => 3,
-    logoutput => true,
-    timeout   => 0,
-    unless    => "ceph osd dump | grep -q \"osd.$(sed -nEe 's|${name}\\ .*ceph-([0-9]+).*$|\\1|p' /proc/mounts)\\ up\\ .*\\ in\\ \"",
+  ceph::osd { $device[0]:
+    journal => $device[1],
   }
-
 }
